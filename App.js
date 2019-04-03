f8ef4af9178f25cc0b531095cb132978a06c33d1
Micky <haxx0rmangay@gmail.com>
2019-04-02 23:05:54 -0400

    put screens in their own files

--- App.js
+++ App.js
@@ -3,75 +3,78 @@ import { Button, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 import SocketIOClient from 'socket.io-client'
 
-class HomeScreen extends React.Component {
-  componentDidMount() {
+import HomeScreen from "./screens/HomeScreen"
+import DetailsScreen from "./screens/DetailsScreen"
 
-      function SockIO(){
-        this.socket = SocketIOClient('http://robocock.herokuapp.com', { transports: ['websocket'], jsonp: false });
+// class HomeScreen extends React.Component {
+//   componentDidMount() {
+//
+//       function SockIO(){
+//         this.socket = SocketIOClient('http://robocock.herokuapp.com', { transports: ['websocket'], jsonp: false });
+//
+//         this.connect = function(){
+//           this.socket.connect();
+//         }
+//
+//         this.send = function(name, data = null){
+//           if (data){
+//             this.socket.emit(name, data);
+//           } else {
+//             this.socket.emit(name);
+//           }
+//         }
+//
+//       }
+//
+//       const sio = new SockIO();
+//       global.sio = sio;
+//       sio.connect()
+//       //global.socket = socket;
+//       sio.socket.on('connect', () => {
+//         console.log('connected to socket server');
+//         //socket.emit('ping');
+//       });
+//       sio.socket.on('ping', () => {
+//         console.log('ping');
+//         //console.log(this.props)
+//         //socket.emit('ping');
+//       });
+//       sio.socket.on('no u', () => {
+//         console.log('kys');
+//         //socket.emit('ping');
+//       });
+//       sio.socket.on('gay', () => {
+//         console.log('fuck u werre rite fuck');
+//         //socket.emit('ping');
+//       });
+//       sio.socket.on('pong', (data) => {
+//         console.log(data);
+//         console.log('pong');
+//       });
+//     }
+//
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Home Screen</Text>
+//         <Button
+//           title="Go to Details"
+//           onPress={() => this.props.navigation.navigate('Details')}
+//         />
+//       </View>
+//     );
+//   }
+// }
 
-        this.connect = function(){
-          this.socket.connect();
-        }
-
-        this.send = function(name, data = null){
-          if (data){
-            this.socket.emit(name, data);
-          } else {
-            this.socket.emit(name);
-          }
-        }
-
-      }
-
-      const sio = new SockIO();
-      global.sio = sio;
-      sio.connect()
-      //global.socket = socket;
-      sio.socket.on('connect', () => {
-        console.log('connected to socket server');
-        //socket.emit('ping');
-      });
-      sio.socket.on('ping', () => {
-        console.log('ping');
-        //console.log(this.props)
-        //socket.emit('ping');
-      });
-      sio.socket.on('no u', () => {
-        console.log('kys');
-        //socket.emit('ping');
-      });
-      sio.socket.on('gay', () => {
-        console.log('fuck u werre rite fuck');
-        //socket.emit('ping');
-      });
-      sio.socket.on('pong', (data) => {
-        console.log(data);
-        console.log('pong');
-      });
-    }
-
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    );
-  }
-}
+// class DetailsScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Details Screen</Text>
+//       </View>
+//     );
+//   }
+// }
 
 const RootStack = createStackNavigator(
   {
