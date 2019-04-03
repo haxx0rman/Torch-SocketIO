6e73cce897de892cf87152ec1a3bfaf630e6cfb4
Micky <haxx0rmangay@gmail.com>
2019-04-02 23:39:12 -0400

    basic authflow proof of concept finished

--- App.js
+++ App.js
@@ -1,82 +1,16 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
+import { createStackNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation'; // Version can be specified in package.json
 import SocketIOClient from 'socket.io-client'
 
-import HomeScreen from "./screens/HomeScreen"
-import DetailsScreen from "./screens/DetailsScreen"
+import SignInScreen from "./screens/SignIn"
+import AuthLoadingScreen from "./screens/AuthLoad"
 
-// class HomeScreen extends React.Component {
-//   componentDidMount() {
-//
-//       function SockIO(){
-//         this.socket = SocketIOClient('http://robocock.herokuapp.com', { transports: ['websocket'], jsonp: false });
-//
-//         this.connect = function(){
-//           this.socket.connect();
-//         }
-//
-//         this.send = function(name, data = null){
-//           if (data){
-//             this.socket.emit(name, data);
-//           } else {
-//             this.socket.emit(name);
-//           }
-//         }
-//
-//       }
-//
-//       const sio = new SockIO();
-//       global.sio = sio;
-//       sio.connect()
-//       //global.socket = socket;
-//       sio.socket.on('connect', () => {
-//         console.log('connected to socket server');
-//         //socket.emit('ping');
-//       });
-//       sio.socket.on('ping', () => {
-//         console.log('ping');
-//         //console.log(this.props)
-//         //socket.emit('ping');
-//       });
-//       sio.socket.on('no u', () => {
-//         console.log('kys');
-//         //socket.emit('ping');
-//       });
-//       sio.socket.on('gay', () => {
-//         console.log('fuck u werre rite fuck');
-//         //socket.emit('ping');
-//       });
-//       sio.socket.on('pong', (data) => {
-//         console.log(data);
-//         console.log('pong');
-//       });
-//     }
-//
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Home Screen</Text>
-//         <Button
-//           title="Go to Details"
-//           onPress={() => this.props.navigation.navigate('Details')}
-//         />
-//       </View>
-//     );
-//   }
-// }
+import HomeScreen from "./screens/Home"
+import DetailsScreen from "./screens/Details"
 
-// class DetailsScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Details Screen</Text>
-//       </View>
-//     );
-//   }
-// }
 
-const RootStack = createStackNavigator(
+const AppStack = createStackNavigator(
   {
     Home: HomeScreen,
     Details: DetailsScreen,
@@ -86,7 +20,18 @@ const RootStack = createStackNavigator(
   }
 );
 
-const AppContainer = createAppContainer(RootStack);
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+const AppContainer = createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
 
 export default class App extends React.Component {
   render() {
---
+++ screens/SignIn.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  AsyncStorage,
+  TouchableOpacity,
+  Button,
+  View,
+} from 'react-native';
+
+
+class SignInScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Please sign in',
+  };
+
+  render() {
+    return (
+      <View>
+        <Button title="Sign in!" onPress={this._signInAsync} />
+      </View>
+    );
+  }
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem('userToken', 'abc');
+    this.props.navigation.navigate('App');
+  };
+}
+export default SignInScreen;
+// class HomeScreen extends React.Component {
+//   static navigationOptions = {
+//     title: 'Welcome to the app!',
+//   };
+//
+//   render() {
+//     return (
+//       <View>
+//         <Button title="Show me more of the app" onPress={this._showMoreApp} />
+//         <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
+//       </View>
+//     );
+//   }
+//
+//   _showMoreApp = () => {
+//     this.props.navigation.navigate('Other');
+//   };
+//
+//   _signOutAsync = async () => {
+//     await AsyncStorage.clear();
+//     this.props.navigation.navigate('Auth');
+//   };
+// }
+
+// More code like OtherScreen omitted for brevity
